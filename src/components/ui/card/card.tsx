a4b29770b58073fd920e4b3c6d25711cf52a3c5c
BNj10 <saquinbenjie7@gmail.com>
2025-03-09 22:10:01 +0800

    Add react-animated-weather file

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import ReactAnimatedWeather from 'react-animated-weather';
-
+    
 interface InputProps {
     temperature: number;
     location: string;
